3dcf46ee76f412fce9fe70bacf108bf8ada160f7
shlomob <149378049+shlomo-b@users.noreply.github.com>
2024-09-27 16:11:35 +0300

    eks

---
+++ main-infra/provider.tf
@@ -0,0 +1,20 @@
+#---------------REGION-------------------#
+
+# Provisers 
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+
+    }
+
+  }
+  required_version = ">=1.5.6"
+}
+
+
+provider "aws" {
+  region = var.aws_region[1]
+}
