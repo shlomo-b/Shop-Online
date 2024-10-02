4dcf030c8d0b7ecd3bd11c34add03d34a42fe520
shlomob <149378049+shlomo-b@users.noreply.github.com>
2024-10-02 17:27:53 +0300

    add role iam

--- main-infra/eks.tf
+++ main-infra/eks.tf
@@ -6,20 +6,18 @@ module "eks" {
   cluster_version = "1.30"
   cluster_service_ipv4_cidr = "10.200.0.0/16"
   cluster_endpoint_public_access  = true
-
- # cluster_endpoint_private_access = true
-
+ 
   # Disable creation of security groups
   create_cluster_security_group = false
   create_node_security_group    = false
   cluster_security_group_id = aws_security_group.sgs["vpc-one"].id
+  
   cluster_addons = {
     coredns                = {}
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
   }
-
     vpc_id                   =  module.vpc["vpc-one"].vpc_id
     subnet_ids               = [
         module.vpc["vpc-one"].public_subnets[0],
@@ -30,13 +28,19 @@ module "eks" {
         module.vpc["vpc-one"].private_subnets[1]
     ]
     
-    # create_iam_role = false
-    # iam_role_name = "aws_iam_role.eks_cluster_role"
+    create_iam_role = false
+    iam_role_name = "eks-cluster-role"
+    iam_role_arn = "arn:aws:iam::148088962203:role/eks-cluster-role"
 
-  # EKS Managed Node Group(s)
+  # EKS Managed Node Group(s) 
   eks_managed_node_group_defaults = {
     instance_types = ["t3.medium"]
 
+    # create self iam role
+    create_iam_role = false
+    iam_role_name = "eks-cluster-role"
+    iam_role_arn = "arn:aws:iam::148088962203:role/eks-cluster-role"
+    
   }
 
   eks_managed_node_groups = {
@@ -49,6 +53,7 @@ module "eks" {
       max_size     = 5
       desired_size = 1
       vpc_security_group_ids = [aws_security_group.sgs["vpc-one"].id]
+      key_name = "TF-key"
     }
   }
 
@@ -73,8 +78,7 @@ module "eks" {
       }
     }
   }
-  
-    
+   
   tags = {
     Environment = "dev"
     Terraform   = "true"
