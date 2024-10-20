b88790e63945666a6773c2ce4f71aef4567e3df8
shlomob <149378049+shlomo-b@users.noreply.github.com>
2024-10-20 16:15:34 +0300

    update eks filies

--- main-infra/seurity-groups.tf
+++ main-infra/seurity-groups.tf
@@ -1,48 +1,5 @@
 #---------------security_group-------------------#
 
-# resource "aws_security_group" "sgs" {
-#   for_each                = var.sgs
-
-#   name   = each.key # each.key = vpc_name
-#   vpc_id = data.aws_vpc.vpcs_name[each.key].id # to work with vpc name and not vpc id
-  
-#   tags = {
-#     name = each.value.tags.name
-#   }
-
-#  dynamic "ingress" { # 
-#   for_each = each.value.ingress # for each value in ingress | the dynamic is used for create some values in ingress
-
-#   content {
-#     # for each value in content ingress
-
-#     description = ingress.value.description
-#     from_port   = ingress.value.from_port  
-#     to_port     = ingress.value.to_port     
-#     protocol    = ingress.value.protocol    
-#     cidr_blocks = ingress.value.cidr_blocks
-#   }
-# }
-#   egress { # each value in egress
-#     description = each.value.egress.description
-#     from_port   = each.value.egress.from_port
-#     to_port     = each.value.egress.to_port
-#     protocol    = each.value.egress.protocol
-#     cidr_blocks = each.value.egress.cidr_blocks
-#   }
-# }
-
-# # this is for work with vpc name and not vpc id.
-# data "aws_vpc" "vpcs_name" {
-#   for_each = var.sgs
-  
-#   filter {
-#     name   = "tag:Name"
-#     values = [each.value.vpc_name]
-#   }
-# }
-
-
 module "sgs" {
   for_each = var.sgs
 
