b01d84d16cab276431542d3a599b923ab3247be9
shlomob <149378049+shlomo-b@users.noreply.github.com>
2024-12-15 23:11:35 +0200

    added new id

--- main-infra/output.tf
+++ main-infra/output.tf
@@ -10,46 +10,13 @@
 #   description = "a record for alb."
 # }
 
-# #---------------all relavte to username-------------------#
-
-# output "user_username" {
-#   value = aws_iam_user_login_profile.credentials.id
-# }
-
-# output "user_alias_name" {
-#   value = data.aws_iam_account_alias.current.account_alias
-# }
-
-# output "user_password" {
-#   value     = aws_iam_user_login_profile.credentials.password
-#   sensitive = true
-# }
-
-# output "user_secret" {
-#   value     = aws_iam_access_key.credentials.secret
-#   sensitive = true
-# }
-
-# output "user_access_key" {
-#   value = aws_iam_access_key.credentials.id
-#}
-
-
-
 #---------------output EC2s id -------------------#
 
 # output "ec2_instances" {
 #   description = "ID of the EC2 instance2"
 #   value = { for i, v in module.ec2_instance_public : i => v.id }
-
 # }
 
-
-#---------------output target_group_arns -------------------#
-
-# output "target_group_arns" {
-#   value = module.alb.target_group_arns.id
-# }
 
 output "dns_a_record_route53" {
   value       = "https://blackjack.spider-shlomo.com"
@@ -63,5 +30,19 @@ output "dns_a_record_route53" {
 
 # output "rds" {
 #   value       = module.db.db_instance_endpoint
+#   description = "rds"
+# }
+
+# output "vpc_public_subnets" {
+#   value = { for k, v in module.vpc : k => v.public_subnets }
+# }
+
+# output "acm" {
+#   value       = { for k, v in module.acm : k => v.acm_certificate_arn }
+#   description = "rds"
+# }
+
+# output "sgs" {
+#   value       = { for k, v in module.sgs : k => v.security_group_id }
 #   description = "rds"
 # }
