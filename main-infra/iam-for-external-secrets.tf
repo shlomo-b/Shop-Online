717c64bb66db910b5bbd2a79c96d15b57baaa8e9
shlomob <149378049+shlomo-b@users.noreply.github.com>
2024-12-21 21:32:14 +0200

    external-secrets

--- main-infra/iam-for-external-secrets.tf
+++ main-infra/iam-for-external-secrets.tf
@@ -1,28 +1,44 @@
 # Create an IAM role for external-secrets
-resource "aws_iam_role" "external_secrets_role" {
-  name = "external-secrets-role"
+# resource "aws_iam_role" "external_secrets_role" {
+#   name = "external-secrets-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com" # Adjust the service based on your use case
-        }
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action    = "sts:AssumeRole"
+#         Effect    = "Allow"
+#         Principal = {
+#           Service = "ec2.amazonaws.com" # Adjust the service based on your use case
+#         }
+#       }
+#     ]
+#   })
 
-  tags = {
-    creator = "external-secrets"
+#   tags = {
+#     creator = "external-secrets"
+#   }
+# }
+
+# # Attach the SecretsManagerReadWrite policy to the role
+# resource "aws_iam_policy_attachment" "external_secrets_policy" {
+#   name       = "external-secrets-policy"
+#   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+#   roles      = [aws_iam_role.external_secrets_role.name]
+# }
+
+module "iam_eks_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "external-secrets-role"
+  
+  role_policy_arns = {
+    policy = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
   }
-}
-
-# Attach the SecretsManagerReadWrite policy to the role
-resource "aws_iam_policy_attachment" "external_secrets_policy" {
-  name       = "external-secrets-policy"
-  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
-  roles      = [aws_iam_role.external_secrets_role.name]
+  
+  oidc_providers = {
+    one = {
+      provider_arn               =  module.eks.oidc_provider_arn
+      namespace_service_accounts = ["external-secrets:external-secrets"]
+    }
+  }
 }
