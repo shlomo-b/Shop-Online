3f85cdaaa948a910c8e368aee0a4383ffe06f1a9
shlomob <149378049+shlomo-b@users.noreply.github.com>
2025-07-09 09:32:07 +0300

    feat: added login feature v1.0.5

--- shop-online-app/frontend/src/App.js
+++ shop-online-app/frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
       setUsername(storedUsername);
     }
     
-    // Check user's preferred color sche5s
+    // Check user's preferred color sche5
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       setIsDarkMode(true);
       document.documentElement.classList.add('dark');
